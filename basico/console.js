afef670259c264febbba98fdb4e9792d654433ed
Edynilson Silva <edynilson.barros@gmail.com>
2020-03-30 17:32:42 -0300

    console

---
+++ basico/console.js
@@ -0,0 +1,29 @@
+console.log('Hello, World!');
+
+// Logging variables
+var foo = "bar";
+console.log(foo);
+
+// Two or more values
+var thisVar = 'first value';
+var thatVar = 'second value';
+console.log("thisVar:", thisVar, "and thatVar:", thatVar);
+
+// Placeholders
+var greet = "Hello", who = "World";
+console.log("%s, $s!", greet, who);
+
+// Logging Objects
+console.log({
+    'Email': '',
+    'Groups': {},
+    'Id': 33,
+    'IsHiddenInUI': false,
+    'IsSiteAdmin': false,
+    'LoginName': 'i:0#.w|virtualdomain\\user2',
+    'PrincipalType': 1,
+    'Title': 'user2'
+});
+
+// Logging HTML elements
+console.log(document.body);

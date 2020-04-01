272341f037f52a36bbc8b3a028b181e461e7ff20
Edynilson Silva <edynilson.barros@gmail.com>
2020-04-01 19:47:42 -0300

    tipos de variáveis em JS

---
+++ variaveis/types.js
@@ -0,0 +1,14 @@
+var myInteger = 12;
+var myLong = 9310141419482;
+var myFloat = 5.5; 
+var myDouble = 9310141419482.22;
+
+var myBoolean = true;
+var myBoolean2 = false;
+
+var myNotANumber = NaN;
+var NaN_Example = 0/0;
+
+var notDefined;
+window.alert(aRandomVariable);
+var myNull = null;

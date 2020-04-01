59cabddd477ae86300d9437b62cd9f25d85e86bd
Edynilson Silva <edynilson.barros@gmail.com>
2020-04-01 19:34:48 -0300

    canvas exemplo

---
+++ basico/canvas.js
@@ -0,0 +1,10 @@
+var canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 250;
+
+var ctx = canvas.getContext('2d');
+
+ctx.font = '30px Cursive';
+ctx.fillText("Hello World", 50, 50);
+
+document.body.appendChild(canvas);

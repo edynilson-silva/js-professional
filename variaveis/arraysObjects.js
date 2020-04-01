2e8fe8a1cfec64c404d3d1b3843f7c5ec51558f3
Edynilson Silva <edynilson.barros@gmail.com>
2020-04-01 20:27:17 -0300

    arrays & objects

---
+++ variaveis/arraysObjects.js
@@ -0,0 +1,34 @@
+// Empty array
+var myArray = [];
+
+// More examples
+
+var favoriteFruits = ["apple", "orange", "strawberry"];
+var carsInParkingLot = ["Toyota", "Ferrari", "Lexus"];
+var employees = ["Billy", "Bob", "Joe"];
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+var randomVariables = [2, "any type works", undefined, null, true, 2.51];
+
+myArray = ["zero", "one", "two"];
+window.alert(myArray[0]); // prints zero
+
+myArray = ["John Doe", "Billy"];
+elementNumber = 1;
+
+window.alert(myArray[elementNumber]); // Billy
+
+
+/* Objects */
+
+myObject = {};
+
+john = {firstname: "John", lastname: "Doe", fullname: "John Doe"};
+
+billy = {
+    firstname: "Billy",
+    lastname: undefined,
+    fullname: "Billy"
+};
+
+window.alert(john.fullname);
+window.alert(billy.fullname);

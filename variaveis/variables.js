0276f6ef3940884f3e4af0bea302b6efbe322b1e
Edynilson Silva <edynilson.barros@gmail.com>
2020-04-01 19:47:18 -0300

    exemplo básico de uma variável e operações matemáticas

---
+++ variaveis/variables.js
@@ -0,0 +1,18 @@
+// Defining a Variable
+
+var myVariable = "This is a variable";
+
+// Using a variable
+var number1 = 5;
+number1 = 3;
+
+// Add, subtract, multiply, divide
+
+number1 = number1 + 5;
+number1 = number1 - 6;
+var number2 = number1 * 10;
+var number3 = number2 / number1;
+
+// Concatenation
+
+var myString = "I am a " + "string!";
